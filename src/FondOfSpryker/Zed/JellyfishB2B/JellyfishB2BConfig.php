c9ca1baa4a288f89f2d0c06432802e2cf52de25a
Daniel Rose <daniel-rose@gmx.de>
2019-03-05 08:10:24 +0100

    config must extend from base module

--- src/FondOfSpryker/Zed/JellyfishB2B/JellyfishB2BConfig.php
+++ src/FondOfSpryker/Zed/JellyfishB2B/JellyfishB2BConfig.php
@@ -2,32 +2,8 @@
 
 namespace FondOfSpryker\Zed\JellyfishB2B;
 
-use FondOfSpryker\Shared\JellyfishB2B\JellyfishB2BConstants;
-use Spryker\Zed\Kernel\AbstractBundleConfig;
+use FondOfSpryker\Zed\Jellyfish\JellyfishConfig;
 
-class JellyfishB2BConfig extends AbstractBundleConfig
+class JellyfishB2BConfig extends JellyfishConfig
 {
-    /**
-     * @return string
-     */
-    public function getBaseUri(): string
-    {
-        return $this->get(JellyfishB2BConstants::BASE_URI, 'http://localhost');
-    }
-
-    /**
-     * @return float
-     */
-    public function getTimeout(): float
-    {
-        return $this->get(JellyfishB2BConstants::TIMEOUT, 2.0);
-    }
-
-    /**
-     * @return bool
-     */
-    public function dryRun(): bool
-    {
-        return $this->get(JellyfishB2BConstants::DRY_RUN, true);
-    }
 }
