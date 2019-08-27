874fb14fe7175cf1c45b65edb30f29b26a9a2ad2
Daniel Rose <daniel-rose@gmx.de>
2019-08-27 17:03:45 +0200

    add missing attributes (priceList, debtorNumber, ...) to jellyfish company

--- src/FondOfSpryker/Zed/JellyfishB2B/Business/Model/Mapper/JellyfishCompanyMapper.php
+++ src/FondOfSpryker/Zed/JellyfishB2B/Business/Model/Mapper/JellyfishCompanyMapper.php
@@ -4,6 +4,7 @@ namespace FondOfSpryker\Zed\JellyfishB2B\Business\Model\Mapper;
 
 use Generated\Shared\Transfer\CompanyTransfer;
 use Generated\Shared\Transfer\JellyfishCompanyTransfer;
+use Generated\Shared\Transfer\JellyfishPriceListTransfer;
 
 class JellyfishCompanyMapper implements JellyfishCompanyMapperInterface
 {
@@ -18,8 +19,34 @@ class JellyfishCompanyMapper implements JellyfishCompanyMapperInterface
         $jellyfishCompany->setId($companyTransfer->getIdCompany())
             ->setUuid($companyTransfer->getUuid())
             ->setExternalReference($companyTransfer->getExternalReference())
-            ->setName($companyTransfer->getName());
+            ->setName($companyTransfer->getName())
+            ->setPriceList($this->mapCompanyToPriceList($companyTransfer))
+            ->setDebtorNumber($companyTransfer->getDebtorNumber())
+            ->setBlockedFor($companyTransfer->getBlockedFor())
+            ->setStatus($companyTransfer->getStatus())
+            ->setIsActive($companyTransfer->getIsActive());
 
         return $jellyfishCompany;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyTransfer $companyTransfer
+     *
+     * @return \Generated\Shared\Transfer\JellyfishPriceListTransfer|null
+     */
+    protected function mapCompanyToPriceList(CompanyTransfer $companyTransfer): ?JellyfishPriceListTransfer
+    {
+        $jellyfishPriceList = new JellyfishPriceListTransfer();
+
+        $priceList = $companyTransfer->getPriceList();
+
+        if ($priceList === null) {
+            return null;
+        }
+
+        $jellyfishPriceList->setIdPriceList($priceList->getIdPriceList())
+            ->setName($priceList->getName());
+
+        return $jellyfishPriceList;
     }
 }
